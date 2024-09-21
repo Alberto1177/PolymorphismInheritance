4025bfac16561d5f04c0bd4eff9cf456bf92bd1e
Alberto Herrera Nieto <alberto.herrera.dev@gmail.com>
2024-09-21 16:43:00 -0600

    Se modifico el main y se checaron los metodos de la clase salesManager

--- src/org/generation/Main.java
+++ src/org/generation/Main.java
@@ -1,6 +1,7 @@
 package org.generation;
 
 import org.generation.classes.Employee;
+import org.generation.classes.SalesManager;
 import org.generation.classes.SalesRep;
 
 public class Main {
@@ -17,15 +18,21 @@ public class Main {
 		Employee maria = new Employee("Maria","Garcia", 103,
 				35, 3600, 5, 12000.0, 10);
 		
-		//Tiempo de jubilacion
+		//Imprimir empleado1,tiempo de jubilacion y vacaciones
+		System.out.println(pablo.toString());
 		System.out.println(pablo.timeToRetirement());
-		System.out.println(marcos.timeToRetirement());
-		System.out.println(maria.timeToRetirement());
-		
-		//Tiempo restante de vacaciones
 		System.out.println(pablo.vacationTimeLeft());
+		
+		//Imprimir empleado2,tiempo de jubilacion y vacaciones
+		System.out.println(marcos.toString());
+		System.out.println(marcos.timeToRetirement());
 		System.out.println(marcos.vacationTimeLeft());
+
+		//Imprimir empleado3,tiempo de jubilacion y vacaciones
+		System.out.println(maria.toString());
+		System.out.println(maria.timeToRetirement());
 		System.out.println(maria.vacationTimeLeft());
+		
 		
 		//Bonus por empleado
 		System.out.println(pablo.calculateBonus());
@@ -42,10 +49,32 @@ public class Main {
 		SalesRep maria3 = new SalesRep("Maria","Garcia", 103,
 				35, 3600, 5, 12000.0, 10, 6700);
 		
+		//Imprimir empleados
+		System.out.println(pablo1.toString());
+		System.out.println(marcos2.toString());
+		System.out.println(maria3.toString());
+		
 		//Comisiones
 		System.out.println(pablo1.calculateComission());
 		System.out.println(marcos2.calculateComission());
 		System.out.println(maria3.calculateComission());
+		
+		//Creacion de manager
+		SalesManager gerente = new SalesManager("Juan", "Lopez", 104, 
+												 50, 6000, 10, 30000.50, 20, 80000);
+		
+		//Imprimir manager
+		System.out.println(gerente.toString());
+		
+		//Agregar representantes del equipo
+		gerente.addSalesRep(pablo1);
+	    gerente.addSalesRep(marcos2);
+	    gerente.addSalesRep(maria3);
+	    
+	    System.out.println("Comision total del equipo: " + gerente.calculateComission());
+	    
+		
+		
 
 	}
 
